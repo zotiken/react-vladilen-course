150f8f16386ac1956e0cfdc0336390d228f08d77
zotiken <neksklad@mail.ru>
2020-06-11 16:12:32 +0300

    added Redux on pages - Create and List

--- src/redux/ActionType.js
+++ src/redux/ActionType.js
@@ -1,9 +1,15 @@
 export const GET_LIST = "GET_LIST";
 export const SET_LIST = "SET_LIST";
+
 export const INIT_COPY_QUIZ = "INIT_COPY_QUIZ";
 export const INPUT_CREATE_QUIZ = "INPUT_CREATE_QUIZ";
 export const ON_CLICK_BUTTON = "ON_CLICK_BUTTON";
 export const SUBMIT_QUIZ = "SUBMIT_QUIZ";
+
+export const INPUT_AUTH = "INPUT_CREATE_QUIZ";
+export const LOGIN = "INPUT_CREATE_QUIZ";
+export const SIGNIN = "INPUT_CREATE_QUIZ";
+export const LOGOUT = "INPUT_CREATE_QUIZ";
 
 
 
